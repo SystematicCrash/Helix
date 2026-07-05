4001788e4df1ac0d45e39bd27d8d6a1286ffc3ba
SystematicCrash <sadraizady@gmail.com>
2026-07-05 23:07:49 +0330

    chore: update entry point to use class-based TCP API (#3)

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
-import {listen, accept, serveClient} from "./tcp/listener";
+import TCPListener from "./network/tcp/TCPListener";
+import {serveClient} from "./network/tcp/server";
 
 async function main() {
-    const listener = listen(1234);
-    const conn = await accept(listener);
+    const listener = new TCPListener();
+    listener.listen(1234);
+    const conn = await listener.accept();
     await serveClient(conn);
 }
 main().catch(console.error);
