ee91b698ae2cc441853192f6a4c801cc72ab7f96
SystematicCrash <sadraizady@gmail.com>
2026-07-15 00:40:08 +0330

    test: add tests for request related functions (#8)

---
+++ tests/unit/network/http/request.test.ts
@@ -0,0 +1,57 @@
+import {beforeEach, afterEach, test, describe, expect} from "vitest";
+import {getReader, parseRequest} from "../../../../src/network/http/request.js";
+import HttpError from "../../../../src/network/http/HttpError.js";
+import {getMockedConnection} from "../common/utils.js";
+import DynamicBuffer from "../../../../src/network/mem/DynamicBuffer.js";
+import {HttpRequest} from "../../../../src/network/http/types.js";
+
+describe("Request", () => {
+
+    describe("parseRequest()", () => {
+
+        test("should wrap raw request bytes to HttpRequest object", () => {
+            const rawRequest = Buffer.from("POST /user/messages HTTP/1.1\r\nHost: example.com");
+            const parsed = parseRequest(rawRequest);
+
+            expect(parsed).toMatchObject({
+                method: "POST",
+                url: "/user/messages",
+                version: "HTTP/1.1",
+                headers: new Map([['host', 'example.com']]),
+            })
+        });
+
+        test("should throw when http method is invalid", () => {
+            const rawRequest = Buffer.from("METHOD /user/messages HTTP/1.1\r\nHost: example.com");
+            expect(() => parseRequest(rawRequest))
+                .toThrow(new HttpError(405, 'Method not allowed'));
+        });
+
+        test("should throw when http method is invalid", () => {
+            const rawRequest = Buffer.from("POST /user/messages HTTP/3\r\nHost: example.com");
+            expect(() => parseRequest(rawRequest))
+                .toThrow(new HttpError(501, 'Http version not supported. supported version: 1.1'));
+        });
+    });
+
+    describe("getReader()", () => {
+
+        test("should return fixed body reader when content-length is set", () => {
+            const request: HttpRequest = {
+                method: "POST",
+                url: "/user/messages",
+                version: "HTTP/1.1",
+                headers: new Map([['content-length', '1000']]),
+            };
+            const reader = getReader(getMockedConnection(), new DynamicBuffer(), request);
+            expect(reader).toMatchObject({
+                length: 1000,
+            });
+        });
+
+        test.todo("should return chunked body reader when transfer-encoding is set to chunked", () => {});
+
+        test.todo("should return body reader until EOF when nothing is set", () => {});
+    });
+
+});
