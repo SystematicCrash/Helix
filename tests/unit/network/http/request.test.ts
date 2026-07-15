4f8891ca6706da9e0bfd67b4fd53b896edc9dd97
mohammadsadra.izady <mohammadsadra.izady@partsoftware.com>
2026-07-15 09:16:16 +0330

    refactor: improve request tests (#8)

--- tests/unit/network/http/request.test.ts
+++ tests/unit/network/http/request.test.ts
@@ -1,33 +1,36 @@
 import {beforeEach, afterEach, test, describe, expect} from "vitest";
 import {getReader, parseRequest} from "../../../../src/network/http/request.js";
 import HttpError from "../../../../src/network/http/HttpError.js";
-import {getMockedConnection} from "../common/utils.js";
+import {mockedTCPConnection} from "../common/utils.js";
 import DynamicBuffer from "../../../../src/network/mem/DynamicBuffer.js";
 import {HttpRequest} from "../../../../src/network/http/types.js";
 
-describe("Request", () => {
+describe("Http Request", () => {
 
     describe("parseRequest()", () => {
 
         test("should wrap raw request bytes to HttpRequest object", () => {
-            const rawRequest = Buffer.from("POST /user/messages HTTP/1.1\r\nHost: example.com");
+            const rawRequest = Buffer.from("POST /user/messages HTTP/1.1\r\nHost: example.com\r\nCustom: something");
             const parsed = parseRequest(rawRequest);
 
             expect(parsed).toMatchObject({
                 method: "POST",
                 url: "/user/messages",
                 version: "HTTP/1.1",
-                headers: new Map([['host', 'example.com']]),
+                headers: new Map([
+                    ['host', 'example.com'],
+                    ['custom', 'something']
+                ]),
             })
         });
 
         test("should throw when http method is invalid", () => {
-            const rawRequest = Buffer.from("METHOD /user/messages HTTP/1.1\r\nHost: example.com");
+            const rawRequest = Buffer.from("INVALID /user/messages HTTP/1.1\r\nHost: example.com");
             expect(() => parseRequest(rawRequest))
                 .toThrow(new HttpError(405, 'Method not allowed'));
         });
 
-        test("should throw when http method is invalid", () => {
+        test("should throw when http version is not supported", () => {
             const rawRequest = Buffer.from("POST /user/messages HTTP/3\r\nHost: example.com");
             expect(() => parseRequest(rawRequest))
                 .toThrow(new HttpError(501, 'Http version not supported. supported version: 1.1'));
@@ -43,7 +46,7 @@ describe("Request", () => {
                 version: "HTTP/1.1",
                 headers: new Map([['content-length', '1000']]),
             };
-            const reader = getReader(getMockedConnection(), new DynamicBuffer(), request);
+            const reader = getReader(mockedTCPConnection(), new DynamicBuffer(), request);
             expect(reader).toMatchObject({
                 length: 1000,
             });
