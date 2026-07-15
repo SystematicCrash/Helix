0a47a07119d0a3f9337f6da93bb0eb348e44fd0f
mohammadsadra.izady <mohammadsadra.izady@partsoftware.com>
2026-07-15 11:34:24 +0330

    test: add unit test cases for DynamicBuffer methods (#11)

--- tests/unit/network/mem/DynamicBuffer.test.ts
+++ tests/unit/network/mem/DynamicBuffer.test.ts
@@ -1,5 +1,6 @@
 import {beforeEach, afterEach, expect, test, describe} from "vitest";
 import DynamicBuffer from "../../../../src/network/mem/DynamicBuffer.js";
+import {push} from "node:stream/iter";
 
 describe("DynamicBuffer", () => {
     describe("push()", () => {
@@ -63,6 +64,77 @@ describe("DynamicBuffer", () => {
             const second = Buffer.from('second data chunk');
             buffer.push(second);
             expect(buffer.length).toEqual(first.length + second.length);
+        });
+    });
+
+    describe("pop()", () => {
+
+        test("should throw when requested length is more that the current buffer length", () => {
+             const buffer = new DynamicBuffer();
+             const data = Buffer.from('This is the data');
+             buffer.push(data);
+
+             expect(() => buffer.pop(data.length + 1))
+                 .toThrow(new Error(`Cannot pop ${buffer.length + 1} bytes, only ${buffer.length} is available!`));
+        });
+
+        test("should pop the requests data length from buffer", () => {
+            const buffer = new DynamicBuffer();
+            const data = Buffer.from('This is the data');
+            buffer.push(data);
+
+            buffer.pop(10);
+            expect(buffer.length).toEqual(data.length - 10);
+        });
+
+        test("should not compact the popped data when it's too small", () => {
+            const buffer = new DynamicBuffer();
+            const data = Buffer.from('This is the data');
+            buffer.push(data);
+
+            buffer.pop(10);
+            expect(buffer.start).toEqual(10);
+            expect(buffer.end).toEqual(data.length);
+        });
+
+        test("should compact buffer when popped data is too large", () => {
+            const buffer = new DynamicBuffer();
+            const data = Buffer.from("x".repeat(32));
+            buffer.push(data);
+
+            expect(buffer.capacity).toEqual(32);
+            buffer.pop(30);
+            expect(buffer.start).toEqual(0);
+            expect(buffer.end).toEqual(2);
+        });
+    });
+
+    describe("cutUntil()", () => {
+
+        test("should cut requested data length", () => {
+            const buffer = new DynamicBuffer(Buffer.from('This is the data'));
+
+            const data = buffer.cutUntil(4);
+            expect(data.toString()).toEqual('This');
+        });
+
+        test("should throw when requested data length is more than the available data length", () => {
+            const buffer = new DynamicBuffer();
+            const data = Buffer.from('This is the data');
+            buffer.push(data);
+
+            expect(() => buffer.cutUntil(data.length + 1))
+                .toThrow(new Error(`Cannot cut ${data.length + 1} bytes, only ${data.length} is available!`));
+        });
+
+        test("should cut just the unconsumed data", () => {
+            const buffer = new DynamicBuffer();
+            const data = Buffer.from('This is the data');
+            buffer.push(data);
+
+            buffer.pop(10);
+            const cut = buffer.cutUntil(buffer.length);
+            expect(cut.length).lessThan(data.length);
         });
     });
 });
