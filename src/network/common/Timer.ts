e0753e5e7d6979ab2e3e5299012f22b6a51d7a4a
mohammadsadra.izady <mohammadsadra.izady@partsoftware.com>
2026-07-20 15:55:20 +0330

    feat: set timer to null after the timeouts fired (#16)

--- src/network/common/Timer.ts
+++ src/network/common/Timer.ts
@@ -1,23 +1,26 @@
-import {EventEmitter} from "events";
-
 export default class Timer {
-    private timer: NodeJS.Timeout|null = null;
-    private emitter: EventEmitter = new EventEmitter();
+    private timer: NodeJS.Timeout | null = null;
 
     constructor(
         private event: string,
         private timeout: number,
         private handler: () => void
-    ) {
-        this.emitter.on(this.event, this.handler);
-    }
+    ) {}
 
     start(): void {
-        this.timer = setTimeout(() => this.emitter.emit(this.event), this.timeout);
+        if (!this.timer) {
+            this.timer = setTimeout(() => {
+                this.timer = null;
+                this.handler();
+            }, this.timeout);
+        }
     }
 
     stop(): void {
-        if (this.timer) clearTimeout(this.timer);
+        if (this.timer) {
+            clearTimeout(this.timer);
+            this.timer = null;
+        }
     }
 
     reset(): void {
