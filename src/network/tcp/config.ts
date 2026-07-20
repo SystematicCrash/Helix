49ce034b00f27014d401d3d26c485a5c24c6bcae
mohammadsadra.izady <mohammadsadra.izady@partsoftware.com>
2026-07-20 15:53:02 +0330

    refactor: add tcp events to the constants.ts (#16)

--- src/network/tcp/config.ts
+++
@@ -1,3 +0,0 @@
-export const IDLE_TIMEOUT = 10_000;
-export const READ_TIMEOUT = 10_000;
-export const WRITE_TIMEOUT = 10_000;
---
+++ src/network/tcp/constants.ts
@@ -0,0 +1,16 @@
+export const IDLE_TIMEOUT = 10_000;
+export const READ_TIMEOUT = 10_000;
+export const WRITE_TIMEOUT = 10_000;
+
+export const EOF = Buffer.alloc(0);
+
+export const events = {
+    idleTimeout: 'idle-timeout',
+    readTimeout: 'read-timeout',
+    writeTimeout: 'write-timeout',
+    error: 'error',
+    data: 'data',
+    end: 'end',
+    write: 'write',
+    close: 'close',
+};
