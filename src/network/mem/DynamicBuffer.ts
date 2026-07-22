2224b0bb4ea75b9792e7b333208647effeb7251f
SystematicCrash <mohammadsadra.izady@partsoftware.com>
2026-07-22 15:36:07 +0330

    feat: add DynamicBuffer size threshold (#20)

--- src/network/mem/DynamicBuffer.ts
+++ src/network/mem/DynamicBuffer.ts
@@ -1,3 +1,5 @@
+import {MAX_BUFFER_SIZE} from "./constants.js";
+
 /** A growable buffer with a sliding window to avoid redundant copies on consume. */
 export default class DynamicBuffer {
     private _length: number;
@@ -42,6 +44,10 @@ export default class DynamicBuffer {
 
     /** Appends data to the buffer, doubling its capacity when the current allocation is exceeded. */
     public push(data: Buffer): void {
+        if (this.length >= MAX_BUFFER_SIZE) {
+            throw new Error('Buffer maximum size exceeded!');
+        }
+
         const newLen = this.length + data.length;
 
         if (newLen > this._capacity) {
