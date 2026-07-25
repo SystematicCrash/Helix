fc2a2fe4bf997e74937b720c9365d0cb88d5444c
SystematicCrash <mohammadsadra.izady@partsoftware.com>
2026-07-25 17:10:33 +0330

    feat: add from and is utility methods to the TCPError.ts (#22)

--- src/network/tcp/TCPError.ts
+++ src/network/tcp/TCPError.ts
@@ -4,4 +4,12 @@ export default class TCPError extends Error {
     constructor(readonly code: TCPCode) {
         super(code.toString());
     }
+
+    static from(code: TCPCode): TCPError {
+    return new TCPError(code);
+    }
+
+    static is(err: Error, code: TCPCode): boolean {
+        return (err instanceof TCPError) && (err.code === code);
+    }
 }
