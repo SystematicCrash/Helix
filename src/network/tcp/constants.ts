a7d14d58901d88679d23744b92d65a19f0521db5
SystematicCrash <mohammadsadra.izady@partsoftware.com>
2026-07-25 17:04:00 +0330

    refactor: move TCPCode from TCPError.ts to constants.ts (#22)

--- src/network/tcp/constants.ts
+++ src/network/tcp/constants.ts
@@ -13,3 +13,17 @@ export enum Event {
     CLOSE = 'close',
     DRAIN = 'drain',
 }
+
+export enum TCPCode {
+    IDLE_TIMEOUT = 'TCP connection lifetime exceeded',
+    WRITE_TIMEOUT = 'TCP write timeout exceeded',
+    READ_TIMEOUT = 'TCP read timeout exceeded',
+    UNKNOWN_TIMEOUT = 'Unexpected connection timeout',
+    UNEXPECTED_CLOSE = 'Connection closed unexpectedly',
+    READ_FROM_CLOSED_CONNECTION = 'Cannot read from a closed connection',
+    WRITE_TO_CLOSED_CONNECTION = 'Cannot write to a closed connection',
+    WRITE_BACKPRESSURE = 'Send buffer is full; cannot write to connection',
+    EMPTY_DATA_BUFFER = 'Data length must be greater than zero',
+    SIMULTANEOUS_READ = 'Another read is already in progress',
+    UNEXPECTED_ERROR = 'Unexpected error occurred',
+}
