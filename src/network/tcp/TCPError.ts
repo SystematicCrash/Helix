df74926a0ab2ef01d785925ac2a5db210c4fc976
SystematicCrash <mohammadsadra.izady@partsoftware.com>
2026-07-26 10:04:09 +0330

    feat: add cause field to TCPError.ts to store the orginal error (#22)

--- src/network/tcp/TCPError.ts
+++ src/network/tcp/TCPError.ts
@@ -1,12 +1,14 @@
 import {TCPCode} from "./constants.js";
 
 export default class TCPError extends Error {
-    constructor(readonly code: TCPCode) {
-        super(code.toString());
+    constructor(
+        readonly code: TCPCode,
+        readonly cause?: Error) {
+        super(cause?.message ?? code.toString());
     }
 
-    static from(code: TCPCode): TCPError {
-    return new TCPError(code);
+    static from(code: TCPCode, cause?: Error): TCPError {
+        return new TCPError(code, cause);
     }
 
     static is(err: Error, code: TCPCode): boolean {
