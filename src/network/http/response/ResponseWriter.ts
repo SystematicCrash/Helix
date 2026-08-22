172e83aa7a8bf2b4a636580e033d8210d737a595
SystematicCrash <sadraizady@gmail.com>
2026-08-22 18:29:37 +0330

    refactor: split response layer into writer and pure helpers (#44)

---
+++ src/network/http/response/ResponseWriter.ts
@@ -0,0 +1,63 @@
+import TCPConnection from "../../tcp/conn/TCPConnection.js";
+import Delimiter from "../../common/constants.js";
+import {HttpHeader, TransferEncoding} from "../common/constants.js";
+import {BodyReader, HttpResponse} from "../common/types.js";
+import {encodeHeaders} from "./encodeHeaders.js";
+
+/*
+ * Serializes and streams HTTP responses to a connection.
+ * Picks fixed-length or chunked framing depending on the body length.
+ */
+export class ResponseWriter {
+    /*
+     * Writes the response headers and streams the body to the connection.
+     */
+    static async write(conn: TCPConnection, response: HttpResponse): Promise<void> {
+        if (response.body.length === -1) {
+            response.headers.set(HttpHeader.TransferEncoding, TransferEncoding.CHUNKED);
+        } else {
+            response.headers.set(HttpHeader.ContentLength, response.body.length.toString());
+        }
+        await conn.write(encodeHeaders(response));
+
+        if (response.headers.get(HttpHeader.TransferEncoding) === TransferEncoding.CHUNKED) {
+            await this.chunkedWriter(conn, response.body);
+        } else {
+            await this.fixedWriter(conn, response.body);
+        }
+        await conn.flush();
+    }
+
+    /*
+     * Streams a body of known length by writing each read chunk directly.
+     */
+    private static async fixedWriter(conn: TCPConnection, body: BodyReader): Promise<void> {
+        while (true) {
+            const data = await body.read();
+            if (!data) break;
+            await conn.write(data);
+        }
+    }
+
+    /*
+     * Streams a body using chunked transfer encoding.
+     */
+    private static async chunkedWriter(conn: TCPConnection, body: BodyReader): Promise<void> {
+        while (true) {
+            const data = await body.read();
+            if (!data) break;
+
+            const chunk = Buffer.from(
+                data.length.toString(16) +
+                Delimiter.CRLF +
+                data +
+                Delimiter.CRLF
+            );
+
+            await conn.write(chunk);
+        }
+
+        const chunk = Buffer.from(0 + Delimiter.CRLF + Delimiter.CRLF);
+        await conn.write(chunk);
+    }
+}
---
+++ src/network/http/response/encodeHeaders.ts
@@ -0,0 +1,15 @@
+import {HTTP_STATUS} from "../common/constants.js";
+import {HttpResponse} from "../common/types.js";
+
+/** Serializes the response status line and headers into a buffer. */
+export function encodeHeaders(response: HttpResponse): Buffer {
+    const parts: string[] = [];
+    parts.push(`${response.version} ${response.code} ${HTTP_STATUS[response.code]}`);
+
+    for (const header of response.headers) {
+        parts.push(`${header[0]}: ${header[1]}`);
+    }
+
+    parts.push('\r\n');
+    return Buffer.from(parts.join("\r\n"));
+}
