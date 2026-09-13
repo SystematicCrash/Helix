8301ecf47afcc5dbf81605e23ef62f35f77c25c9
SystematicCrash <mohammadsadra.izady@partsoftware.com>
2026-09-13 09:16:55 +0330

    refactor: remove unnecessary imports from TCPConnection.ts (#50)

--- src/network/tcp/conn/TCPConnection.ts
+++ src/network/tcp/conn/TCPConnection.ts
@@ -6,10 +6,7 @@ import Timer from "../../common/Timer.js";
 import {
     Event,
     IDLE_TIMEOUT,
-    MAX_WRITE_BUFFER_SIZE,
-    READ_TIMEOUT,
     TCPErrCode,
-    WRITE_TIMEOUT,
 } from "../common/constants.js";
 
 /**
