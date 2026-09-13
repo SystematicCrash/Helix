b897cdc6ab8566910ea1f7d6ac42d4e8744a5681
SystematicCrash <mohammadsadra.izady@partsoftware.com>
2026-09-13 10:35:47 +0330

    test(http): cover notFound rendering and sanitized 500 bodies (#50)
    Update mapErrorToResponse tests for the new (error, request, info) signature
    and add a test verifying that 404 responses render the notFound template.

--- tests/unit/network/http/response.test.ts
+++ tests/unit/network/http/response.test.ts
@@ -3,17 +3,27 @@ import HttpError from '../../../../src/network/http/common/HttpError.js';
 import { mapErrorToResponse } from '../../../../src/network/http/response/mapErrorToResponse.js';
 import { ResponseWriter } from '../../../../src/network/http/response/ResponseWriter.js';
 import { HttpVersion } from '../../../../src/network/http/common/constants.js';
-import { HttpResponse } from '../../../../src/network/http/common/types.js';
+import { HttpRequest, HttpResponse } from '../../../../src/network/http/common/types.js';
 import MemoryBodyReader from '../../../../src/network/http/request/body/MemoryBodyReader.js';
 import { mockedTCPConnection } from '../common/utils.js';
 import {TCPConnection} from '../../../../src/network/tcp';
+import {ServerInfo} from '../../../../src/server/ServerInfo.js';
+
+const PLACEHOLDER_REQUEST: HttpRequest = {
+    method: 'GET',
+    url: '/missing',
+    version: HttpVersion.HTTP_1_1,
+    headers: new Map(),
+};
+
+const INFO: ServerInfo = {port: 1234, iface: '0.0.0.0', version: '1.0.0'};
 
 describe('mapErrorToResponse()', () => {
 
     describe('HttpError mapping', () => {
         test('should map HttpError code and message to response', () => {
             const err = new HttpError(422, 'Cannot parse request body content!');
-            const res = mapErrorToResponse(err);
+            const res = mapErrorToResponse(err, PLACEHOLDER_REQUEST, INFO);
 
             expect(res).toMatchObject({
                 code: 422,
@@ -23,42 +33,56 @@ describe('mapErrorToResponse()', () => {
             });
         });
 
+        test('should render notFound template on 404', async () => {
+            const err = new HttpError(404, 'Not found');
+            const res = mapErrorToResponse(err, PLACEHOLDER_REQUEST, INFO);
+
+            expect(res.code).toBe(404);
+            expect(res.body.length).toBeGreaterThan(0);
+            const data = await res.body.read();
+            const html = data?.toString('utf-8') ?? '';
+            expect(html).toContain('404 Not Found');
+            expect(html).toContain(PLACEHOLDER_REQUEST.url);
+            expect(html).toContain(PLACEHOLDER_REQUEST.method);
+            expect(html).toContain(INFO.version);
+        });
+
         test('should use HttpError status code directly', () => {
             const err = new HttpError(404, 'Not found');
-            const res = mapErrorToResponse(err);
+            const res = mapErrorToResponse(err, PLACEHOLDER_REQUEST, INFO);
 
             expect(res.code).toBe(404);
         });
     });
 
     describe('generic Error mapping', () => {
-        test('should map Error to 500 response', () => {
-            const err = new Error('Something went wrong!');
-            const res = mapErrorToResponse(err);
+        test('should map Error to 500 response with sanitized body', () => {
+            const err = new Error('Internal stack-trace leak');
+            const res = mapErrorToResponse(err, PLACEHOLDER_REQUEST, INFO);
 
             expect(res).toMatchObject({
                 code: 500,
                 version: HttpVersion.HTTP_1_1,
                 headers: new Map(),
-                body: { length: err.message.length },
+                body: { length: 'Internal Server Error'.length },
             });
         });
     });
 
     describe('unknown error mapping', () => {
         test('should map non-error object to 500 response with default message', () => {
-            const res = mapErrorToResponse({});
+            const res = mapErrorToResponse({}, PLACEHOLDER_REQUEST, INFO);
 
             expect(res).toMatchObject({
                 code: 500,
                 version: HttpVersion.HTTP_1_1,
                 headers: new Map(),
-                body: { length: 'Webserver Internal Error'.length },
+                body: { length: 'Internal Server Error'.length },
             });
         });
 
         test('should map null to 500 response with default message', () => {
-            const res = mapErrorToResponse(null);
+            const res = mapErrorToResponse(null, PLACEHOLDER_REQUEST, INFO);
 
             expect(res).toMatchObject({
                 code: 500,
@@ -68,7 +92,7 @@ describe('mapErrorToResponse()', () => {
         });
 
         test('should map string to 500 response', () => {
-            const res = mapErrorToResponse('something went wrong');
+            const res = mapErrorToResponse('something went wrong', PLACEHOLDER_REQUEST, INFO);
 
             expect(res).toMatchObject({
                 code: 500,
